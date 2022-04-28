631cc0e42d09bb02c94a63a97ca7931d572a94b2
Rahul Bhalley <0behinds_secant@icloud.com>
2022-04-28 20:09:14 +0530

    Log `style.shape`.

--- core/distiller.py
+++ core/distiller.py
@@ -195,6 +195,7 @@ class Distiller(pl.LightningModule):
             img = self.synthesis_net(style)["img"]
         if return_latents:
             print(f"style[0].shape: {style[0].shape}")
+            print(f"style.shape: {style.shape}")
             return img, style[0]
         else:
             return img
